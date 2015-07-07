9f0dd92c897841a4c4bed4f09a25f6d6091180ad
Joey wang <wxluckly@gmail.com>
2015-07-07 15:56:56 +0800

    coding

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   namespace :sub do
-    get :about_us, :business, :contact
+    get :about_us, :business, :contact, :news, :jobs
   end
 
 end
