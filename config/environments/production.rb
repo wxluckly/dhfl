0f9f613d6249f6589430c60a40505652b2cbb594
Joey wang <wxluckly@gmail.com>
2015-11-13 16:41:06 +0800

    cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = Proc.new do |source|
+    "http://#{source =~ /(eot|otf|ttf|woff)$/ ? 'dinghuifl.com' : 'cdn.dinghuifl.smjjl.com'}"
+  end.tap {|p| def p.to_s; end}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
